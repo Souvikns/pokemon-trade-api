1be23ce8a4295c8b8933d8abdcea513a0ba605b7
root <root@LAPTOP-K7PL9FIU>
2024-08-08 20:59:45 +0530

    feat: improve user model

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import http from 'http'
 import { v4 as uuidv4 } from 'uuid'
 
 import { TradeInterface } from './models/trade'
+import { UsersDatabase} from './models/user'
 
 
 const app = express()
@@ -11,18 +12,23 @@ const server = http.createServer(app)
 const io = new Server(server)
 
 const TradeList: Array<TradeInterface> = []
+const userDatabase = new UsersDatabase()
 
 io.on('connection', (socket) => {
-    console.log('Client connected')
+    userDatabase.addUser(socket.id)
 
     socket.on("new-trade", (trade) => {
         TradeList.push({ id: uuidv4(), game: trade.game, pokemon_for_trade: trade.pokemon_for_trade, pokemon_want: trade.pokemon_want })
         socket.emit('trade-list', TradeList)
-        console.log(socket.id)
 
     })
 
+    socket.on('dissconnect', () => {
+        userDatabase.removeUser(socket.id)
+    })
 })
+
+
 
 
 server.listen(8080, () => {
--- src/models/user.ts
+++ src/models/user.ts
@@ -1,7 +1,48 @@
+import { v4 as uuidv4 } from 'uuid'
+import { TradeInterface } from './trade'
+import { uniqueNamesGenerator, adjectives, colors, animals } from 'unique-names-generator'
 
-export interface UserInterface {
-    id: string
-    username: string
-    socket_id: string
+export class User {
+    private id: string
+    private username: string
+    private socket_id: string
+
+    constructor(username: string, socket_id: string) {
+        this.id = uuidv4()
+        this.username = username
+        this.socket_id = socket_id
+    }
+
+    get socketId(): string {
+        return this.socketId
+    }
+
+    addTrade(Trade: TradeInterface) {
+
+    }
+
+
+
+    static generateNewUser(username: string, socket_id: string): User {
+        return new User(username, socket_id)
+    }
 }
 
+export class UsersDatabase {
+    private users: Array<User> = []
+
+    addUser(socket_id: string) {
+        this.users.push(User.generateNewUser(
+            uniqueNamesGenerator({dictionaries: [adjectives, colors, animals], length: 2}),
+            socket_id
+        ))
+    }
+
+    removeUser(socket_id: string) {
+        this.users = this.users.filter(u => {
+            if(u.socketId !=  socket_id) {
+                return u
+            }
+        })
+    }
+}
